ccbdc892eee6b4fe7d6201d3bd70085f9f58e956
Maxim <zmq19970623@outlook.com>
2023-06-22 16:56:27 +1000

    Update urls.py

--- backend/apiproject/urls.py
+++ backend/apiproject/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  
     path('admin/', admin.site.urls),
-    path('menu/', include('blog.urls')),
+    path('hungry/', include('blog.urls')),
 ]
