759c6c94f3a8fa355aeef333494d950abd700248
mac <mac@JinCc.local>
2023-07-25 21:13:28 +1000

    top5 completed

--- frontend/src/components/customer/categorypage.jsx
+++ frontend/src/components/customer/categorypage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { List, Card, Button } from "antd";
+import { List, Card, Button, Badge } from "antd";
 import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 import CartEdit from "./cartEdit";
 
@@ -8,6 +8,9 @@ function CategoryPage() {
     const [dishData, setDishData] = useState([]);
     const [count, setCount] = useState({});
     const [orderData, setOrderData] = useState([]);
+    const [historyOrderData, setHistoryOrderData] = useState([])
+    const [top5Dish, setTop5Dish] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
     let categoryInfo = useParams();
 
     // fetch dishes with specific categories
@@ -77,58 +80,90 @@ function CategoryPage() {
     };
 
     useEffect(() => {
-        let newcount = {};
-        orderData.forEach(item => {
-            newcount[item.DishID] = item.DishAmount || 0
-        });
-        setCount(newcount);
-    }, [orderData])
+        const fetchOrderData = async () => {
+            const response = await fetch('http://localhost:8000/hungry/orders/');
+            const data = await response.json();
+            setHistoryOrderData(data);
+        }
+
+        fetchOrderData()
+    }, [])
+    let dishCounts = {};
+    for (let order of historyOrderData) {
+        for (let dish of order.DishList) {
+            if (dish.DishID in dishCounts) {
+                dishCounts[dish.DishID] += dish.DishAmount;
+            } else {
+                dishCounts[dish.DishID] = dish.DishAmount;
+            }
+        }
+    }
+    let dishCountsArray = Object.entries(dishCounts).map(([DishID, count]) => ({ DishID, count }));
+    dishCountsArray.sort((a, b) => b.count - a.count);
+
+    let top5Dishes = dishCountsArray.slice(0, 5);
 
     return (
         <>
             <List
                 grid={{ gutter: 16, column: 1 }}
                 dataSource={dishData}
-                renderItem={dish => (
-                    <List.Item>
-                        <Card>
-                            <div style={{
-                                display: 'flex', alignItems: 'center',
-                                justifyContent: 'center'
-                            }}>
-                                <img
-                                    alt={dish.DishName}
-                                    src={'/ayouh.jpeg'}
-                                    style={{ height: '200px', objectFit: 'cover' }}
-                                />
-                                <div style={{ marginRight: '40px', marginLeft: '40px' }}>
-                                    <Card.Meta title={dish.DishName} />
-                                    <div className="dish-info">
-                                        <p>Id: {dish.DishID}</p>
-                                        <p className="price">Price: {dish.Price} $</p>
-                                        <p className="description">Description: {dish.Description}</p>
-                                        <p className="ingredients">Ingredients: {dish.Ingredients}</p>
+                renderItem={dish => {
+                    const isTopDish = top5Dishes.some(topDish => Number(topDish.DishID) === dish.DishID);
+                    return (
+                        <List.Item>
+                            <Card>
+                                <div style={{
+                                    display: 'flex', alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}>
+                                    <img
+                                        alt={dish.DishName}
+                                        src={'/ayouh.jpeg'}
+                                        style={{ height: '200px', objectFit: 'cover' }}
+                                    />
+                                    <div style={{ marginRight: '40px', marginLeft: '40px' }}>
+                                        <Card.Meta title={dish.DishName} />
+                                        <div className="dish-info">
+                                            <h3 style={{ marginBottom: 0 }}>
+                                                {isTopDish &&
+                                                    <Badge
+                                                        count="Top Dish"
+                                                        style={{
+                                                            backgroundColor: '#f5222d',
+                                                            color: '#fff',
+                                                            boxShadow: '0 0 0 1px #d9d9d9 inset',
+                                                            marginLeft: '10px',
+                                                        }}
+                                                    />
+                                                }
+                                            </h3>
+                                            <p>Id: {dish.DishID}</p>
+                                            <p className="price">Price: {dish.Price} $</p>
+                                            <p className="description">Description: {dish.Description}</p>
+                                            <p className="ingredients">Ingredients: {dish.Ingredients}</p>
+                                        </div>
+                                    </div>
+                                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                                        <Button
+                                            type="primary"
+                                            shape="circle"
+                                            icon={<PlusOutlined />}
+                                            onClick={() => PlusCount(dish.DishID)}
+                                        />
+                                        <p style={{ margin: '0 16px' }}>{count[dish.DishID] || 0}</p>
+                                        <Button
+                                            type="primary"
+                                            shape="circle"
+                                            icon={<MinusOutlined />}
+                                            onClick={() => MinusCount(dish.DishID)}
+                                        />
                                     </div>
                                 </div>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <Button
-                                        type="primary"
-                                        shape="circle"
-                                        icon={<PlusOutlined />}
-                                        onClick={() => PlusCount(dish.DishID)}
-                                    />
-                                    <p style={{ margin: '0 16px' }}>{count[dish.DishID] || 0}</p>
-                                    <Button
-                                        type="primary"
-                                        shape="circle"
-                                        icon={<MinusOutlined />}
-                                        onClick={() => MinusCount(dish.DishID)}
-                                    />
-                                </div>
-                            </div>
-                        </Card>
-                    </List.Item >
-                )
+                            </Card>
+                        </List.Item >
+                    )
+                }
                 }
             />
 
